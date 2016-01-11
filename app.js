f257310a83e687d4d48c09bc08f1688b9ba9420f
horsekitlin <horsekit1982@gmail.com>
2016-01-11 08:37:30 +0800

    get old post

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ import co from 'co';
 import _ from 'lodash';
 import FBManager from './lib/FBBase';
 import ErrorManager from './lib/ErrorBase';
-import { Fans, Posts } from './models';
-import { getTimeStamp } from './lib/TimeBase';
+import { Fans, Posts, Logs } from './models';
+import { getTimeStamp, now } from './lib/TimeBase';
 
 const fields = 'message,created_time,story,link,full_picture,comments,picture,from';
 
 CronTab.createJob('*/20 * * * * * ', getAllPosts);
+
 function getAllPosts(){
     co(function* (){
         let fanspage = yield Fans.show({
@@ -17,14 +18,14 @@ function getAllPosts(){
                 "$ne" : 'done'
             }
         });
-
         fanspage.lock = true;
         yield fanspage.syncSave();
 
         if(!_.isNull(fanspage)){
             let posts = yield FBManager.getPosts(fanspage.fans_id, {limit : 100,fields : fields });
-            if(!_.isUndefined(posts.paging
-                    && !_.isUndefined(posts.paging.next))){
+
+            if(!_.isUndefined(posts.paging)
+                    && !_.isUndefined(posts.paging.next)){
 
                     const content = yield FBManager.getOldPosts(fanspage.fans_id
                         , posts.paging.next);
@@ -33,9 +34,12 @@ function getAllPosts(){
                     });
 
                     const query = _.map(posts.data, (post) => {
-                        post.created_time = getTimeStamp(post.created_time);
-                        return post;
+                        let content = FBManager.getPostContent(post);
+                        content.post_id = post.id;
+                        content.created_time = getTimeStamp(post.created_time);
+                        return content;
                     });
+
                     fanspage.lock = false;
                     fanspage.nextpage = 'done';
 
@@ -43,6 +47,12 @@ function getAllPosts(){
                         Posts.create(query),
                         fanspage.syncSave()
                     ];
+                    yield Logs.commit({
+                        type : 'addposts',
+                        created_time : now(),
+                        message : 'success',
+                        querys : query
+                    });
 
             }
         }
